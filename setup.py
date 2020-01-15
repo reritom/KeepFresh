6cf10b3cf6da21aaafd185324db84788b83ad030
reritom <reikudjinn@gmail.com>
2020-01-15 22:57:02 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description="Filesystem event polling",
     author="Tomas Sheers",
     author_email="t.sheers@outlook.com",
-    download_url="https://github.com/reritom/KeepFresh/archive/v0.0.1.tar.gz",
+    download_url="https://github.com/reritom/KeepFresh/archive/v0.0.2.tar.gz",
     url="https://github.com/reritom/KeepFresh",
     keywords=["FILESYSTEM", "EVENT", "POLLING"],
     long_description=get_readme(),
